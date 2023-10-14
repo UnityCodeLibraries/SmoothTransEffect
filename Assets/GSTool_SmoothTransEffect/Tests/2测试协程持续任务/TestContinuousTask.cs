97504232e3b483bb002ed04ea5f2d660eb67ee6e
shikeik <30600841+shikeik@users.noreply.github.com>
2023-10-14 14:32:54 +0800

    V0.1.x~ 完成 任务 详情
    [x] 0.1.0
    验证场景: 向目标移动并检测是否越过目标
    
    [x] 0.1.1
    验证场景: 协程持续位移任务
    
    • 开发文档地址:
        https://kdocs.cn/l/ceAofwMb9fVr?linkname=kVs9QEnuNQ
    
    ---

---
+++ Assets/GSTool_SmoothTransEffect/Tests/2测试协程持续任务/TestContinuousTask.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 协程移动到目标位置并停下, 并在Inspector面板有一个进度条<para/>
+/// </summary>
+public class TestContinuousTask : MonoBehaviour
+{
+    public GameObject Origin;
+    public GameObject Target;
+    public bool StartToggle;
+    public bool MovingToggle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartToggle)
+        {
+            StartToggle = false;
+            MovingToggle = true;
+
+            StartCoroutine(MoveTask());
+        }
+    }
+
+
+    public IEnumerator MoveTask()
+    {
+        while (MovingToggle)
+        {
+            MoveAndDetection();
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+
+    Vector3 lastDir;
+    private void MoveAndDetection()
+    {
+        if (!MovingToggle) return;
+
+        //配置
+        var moveRate = 1/60f;
+
+        var selfPos = Origin.transform.position;
+        var targetPos = Target.transform.position;
+        var laterPos = selfPos + new Vector3(moveRate, 0, 0);
+
+        //判断方向
+        var targetDir = targetPos - laterPos;
+        var areOpposite = Vector3.Dot(lastDir, targetDir);
+        if (areOpposite < 0)
+        {
+            Debug.Log("方向相反");
+            MovingToggle = false;
+            laterPos = targetPos;
+        }
+        else
+        {
+            //更新此次运动方向
+            lastDir = targetDir;
+        }
+
+        //移动
+        if(areOpposite<0) Debug.Log($"Move arrive.");
+        else Debug.Log($"Move add x{moveRate}.");
+        Origin.transform.position = laterPos;
+
+    }
+
+
+}
